c15195efc00e9074ffd9af472c1db9074b9ff086
krabiworld <krabiworld@proton.me>
2023-02-21 21:20:08 +0500

    Add system theme changer Fix app theme changing Update .NET to 7

--- WinTheme/MainWindow.xaml.cs
+++ WinTheme/MainWindow.xaml.cs
@@ -16,27 +16,35 @@ public partial class MainWindow
         using var key = Registry.CurrentUser
             .OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize")!;
             
-        var isLightTheme = (int) key.GetValue("AppsUseLightTheme")! == 1;
+        var isLightAppTheme = (int) key.GetValue("AppsUseLightTheme")! == 1;
+        var isLightSystemTheme = (int) key.GetValue("SystemUsesLightTheme")! == 1;
         var transparencyEnabled = (int) key.GetValue("EnableTransparency")! == 1;
 
-        if (isLightTheme)
-        {
-            LightButton.IsChecked = true;
-        }
-        else
-        {
-            DarkButton.IsChecked = false;
-        }
+        var appsThemeButton = isLightAppTheme ? LightAppsButton : DarkAppsButton;
+        appsThemeButton.IsChecked = true;
+        
+        var systemThemeButton = isLightSystemTheme ? LightSystemButton : DarkSystemButton;
+        systemThemeButton.IsChecked = true;
 
         TransparencyButton.IsChecked = transparencyEnabled;
     }
 
-    private void ChangeTheme(object sender, RoutedEventArgs e)
+    private void ChangeAppsTheme(object sender, RoutedEventArgs e)
     {
         var key = Registry.CurrentUser
             .OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", true)!;
 
-        key.SetValue("AppsUseLightTheme", LightButton.IsChecked == true ? 1 : 0);
+        key.SetValue("AppsUseLightTheme", LightAppsButton.IsChecked == true ? 1 : 0);
+        
+        key.Close();
+    }
+    
+    private void ChangeSystemTheme(object sender, RoutedEventArgs e)
+    {
+        var key = Registry.CurrentUser
+            .OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize", true)!;
+
+        key.SetValue("SystemUsesLightTheme", LightSystemButton.IsChecked == true ? 1 : 0);
         
         key.Close();
     }
